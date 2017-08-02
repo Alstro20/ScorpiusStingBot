4f40c270586ca1960ee43c3cc28d40958f5fdb5f
Alstro20 <Alstro20@users.noreply.github.com>
2017-08-02 12:39:37 -0500

    Version 0.0.1
    Very basics of the bot

---
+++ alstrobot.py
@@ -0,0 +1,29 @@
+import discord
+import asyncio
+from discord.enums import Status
+
+client = discord.Client()
+
+prefix = 'a!'
+
+@client.event
+async def on_ready():
+    print('Logging in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith(prefix+'ping'):
+        await client.send_message(message.channel, 'Pong!')
+        print("Pong!")
+        
+    if message.content.startswith(prefix+'shutdown'):
+        await client.send_message(message.channel, 'Shutting down')
+        await client.change_status(None, Status.invisible)
+        await client.close()
+        print("Shutdown")
+    
+
+client.run('MzQyMzM0NzE2MTM5MTQzMTY5.DGOHmA.fHEu372uduE5hwGZ7vjFzS0BmJQ')
