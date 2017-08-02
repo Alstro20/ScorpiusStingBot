0ed1cea1d854e823d616bbd670f5e0412812a0cd
Alstro20 <Alstro20@users.noreply.github.com>
2017-08-02 16:31:57 -0500

    Added "Message Unknown"
    -Message Unknown added to Line 71

--- scorpion.py
+++ scorpion.py
@@ -2,7 +2,6 @@ import discord
 import asyncio
 from discord.enums import Status
 import time
-from _ast import Await
 
 client = discord.Client()
 
@@ -31,36 +30,26 @@ async def on_message(message):
         print("Pong!")
         
     #Command to shut down the bot
-    if message.content.startswith(prefix+'shutdown'):
+    elif message.content.startswith(prefix+'shutdown'):
         await client.send_message(message.channel, 'Shutting down')
         await client.change_presence(game=None,status=None,afk=False)
         #sleep for 2 seconds before shutdown, just to make sure status changed correctly
         time.sleep(2)
         await client.logout()
         print("Shutdown") 
-        
-    #Command to restart the bot
-    if message.content.startswith(prefix+'restart'):
-        print("Restarting")
-        await client.logout()
-        time.sleep(5)
-        #await client.login('MzQyMzc5OTg5MDk1Njc3OTUz.DGOxug.wbSojJmHCDlq6Z0t70za4ZjWyzA') [NON FUNCTIONAL]
-        client.run('MzQyMzc5OTg5MDk1Njc3OTUz.DGOxug.wbSojJmHCDlq6Z0t70za4ZjWyzA')
-        print("Restart complete")
-        await client.send_message(message.channel, "Restart complete")
     
     #Command to ping the bot (again)
-    if message.content.startswith(prefix+'greet'):
+    elif message.content.startswith(prefix+'greet'):
         await client.send_message(message.channel, 'Hello!')
         print("Hello!")        
         
     #Command to DM a user that requests it
-    if message.content.startswith(prefix+'dm'):
+    elif message.content.startswith(prefix+'dm'):
         await client.send_message(message.author, 'Slidin into the DMs ;)')
         print("sent a message")
         
     #Command to generate an invite
-    if message.content.startswith(prefix+'invite'):
+    elif message.content.startswith(prefix+'invite'):
         await client.send_message(message.channel, await client.create_invite(message.channel))
         print("Created server invite")
     
@@ -73,7 +62,13 @@ async def on_message(message):
     elif message.content.startswith(prefix+'info'):
         await client.send_message(message.author, "Scorpion bot is a little bot made by Alstro20 and EmeraldOrbis. Check out the Github project at https://github.com/Alstro20/ScorpiusStingBot")
         await client.send_message(message.channel, message.author, "Send you a DM")
-        print("Info sent to", message.author)   
+        print("Info sent to", message.author) 
+        
+        
+    
+    #Lets user know if script is unknown. Put all commands before this.
+    elif message.content.startswith(prefix):
+        await client.send_message(message.channel, 'Message unknown')  
 
         
 client.run('MzQyMzc5OTg5MDk1Njc3OTUz.DGOxug.wbSojJmHCDlq6Z0t70za4ZjWyzA')
