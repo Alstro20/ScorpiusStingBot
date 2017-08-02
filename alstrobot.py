d0377c7ab04331758d9b17afe8c1af78ca6425c1
Alstro20 <Alstro20@users.noreply.github.com>
2017-08-02 12:56:52 -0500

    Added comments

--- alstrobot.py
+++ alstrobot.py
@@ -6,19 +6,23 @@ client = discord.Client()
 
 prefix = 's!'
 
+#Bot comes online
 @client.event
 async def on_ready():
     print('Logging in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    
+   
+#Bot checks for messages being sent
 @client.event
 async def on_message(message):
+    #Ping
     if message.content.startswith(prefix+'ping'):
         await client.send_message(message.channel, 'Pong!')
         print("Pong!")
         
+    #Command to shut down the bot
     if message.content.startswith(prefix+'shutdown'):
         await client.send_message(message.channel, 'Shutting down')
         await client.change_status(None, Status.invisible)
