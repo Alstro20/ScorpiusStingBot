32dc7a6b217019a908a24d417da2a53f3d509d54
EmeraldOrbis <30666010+EmeraldOrbis@users.noreply.github.com>
2017-08-02 23:00:33 -0500

    updated info command
    It took WAY too long but I did it!

--- ScorpionDev.py
+++ ScorpionDev.py
@@ -9,6 +9,7 @@ client = discord.Client()
 print("Starting bot")
 print("DEV VERSION MIGHT NOT BE STABLE")
 
+
 prefix = 's!'
 
 #Bot comes online
@@ -65,7 +66,7 @@ async def on_message(message):
     #Command to give information about the bot  
     elif message.content.startswith(prefix+'info'):
         await client.send_message(message.author, "Scorpion bot is a little bot made by Alstro20 and EmeraldOrbis. Check out the Github project at https://github.com/Alstro20/ScorpiusStingBot")
-        await client.send_message(message.channel, message.author, "Send you a DM")
+        await client.send_message(message.channel, "Sent you a DM, "+message.author.mention)
         print("Info sent to", message.author) 
         
     #Command to make the bot say whatever
