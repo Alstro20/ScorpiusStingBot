151ab792a75a2bbc1b80b05f13bf094b00998534
Steve Jobs <Alstro20@users.noreply.github.com>
2017-08-02 19:20:38 -0500

    New file for Scorpion.py

---
+++ ScorpionDev.py
@@ -0,0 +1,84 @@
+import discord
+import asyncio
+from discord.enums import Status
+import time
+
+client = discord.Client()
+
+#[---PRE-BOOT---]
+print("Starting bot")
+print("DEV VERSION MIGHT NOT BE STABLE")
+
+prefix = 's!'
+
+#Bot comes online
+@client.event
+async def on_ready():
+    print('Logging in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+   
+#[---COMMANDS---]
+@client.event
+async def on_message(message):
+    #Command's contents are stored in commandContents
+    if message.content.startswith(prefix):
+        commandString = message.content
+        commandContents = commandString.split(' ', 1)[-1]
+        
+    
+    #Command to respond with "Pong!" (For testing)
+    if message.content.startswith(prefix+'ping'):
+        await client.send_message(message.channel, 'Pong!')
+        print("Pong!")
+        
+    #Command to shut down the bot
+    elif message.content.startswith(prefix+'shutdown'):
+        await client.send_message(message.channel, 'Shutting down')
+        await client.change_presence(game=None,status=None,afk=False)
+        #sleep for 2 seconds before shutdown, just to make sure status changed correctly
+        time.sleep(2)
+        await client.logout()
+        print("Shutdown") 
+    
+    #Command to ping the bot (again)
+    elif message.content.startswith(prefix+'greet'):
+        await client.send_message(message.channel, 'Hello!')
+        print("Hello!")        
+        
+    #Command to DM a user that requests it
+    elif message.content.startswith(prefix+'dm'):
+        await client.send_message(message.author, 'Slidin into the DMs ;)')
+        print("sent a message")
+        
+    #Command to generate an invite
+    elif message.content.startswith(prefix+'invite'):
+        await client.send_message(message.channel, await client.create_invite(message.channel))
+        print("Created server invite")
+    
+    #Command to link to a specified subreddit
+    elif message.content.startswith(prefix+'reddit'):
+        await client.send_message(message.channel, 'https://www.reddit.com/r/'+commandContents)
+        print("Reddit")
+      
+    #Command to give information about the bot  
+    elif message.content.startswith(prefix+'info'):
+        await client.send_message(message.author, "Scorpion bot is a little bot made by Alstro20 and EmeraldOrbis. Check out the Github project at https://github.com/Alstro20/ScorpiusStingBot")
+        await client.send_message(message.channel, message.author, "Send you a DM")
+        print("Info sent to", message.author) 
+        
+    #Command to make the bot say whatever
+    elif message.content.startswith(prefix+'say'):
+        await client.delete_message(message)
+        await client.send_message(message.channel, commandContents)
+        print("Said something")
+        
+        
+    
+    #Lets user know if script is unknown. Put all commands before this.
+    elif message.content.startswith(prefix):
+        await client.send_message(message.channel, 'Invalid Command')  
+
+        
+client.run('MzQyMzc5OTg5MDk1Njc3OTUz.DGOxug.wbSojJmHCDlq6Z0t70za4ZjWyzA')
