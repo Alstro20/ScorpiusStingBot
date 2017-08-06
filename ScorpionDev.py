a58cebb65c999b12a289140ee44796e68b54ec5c
Steve Jobs <Alstro20@users.noreply.github.com>
2017-08-05 19:36:32 -0500

    Added lmgtfy and fixed some stuff
    -Added LMGTFY Command
    -(slightly) Changed the way admin commands are handled
    -Added Admin command: changeGame (changes the bot's current game)

--- ScorpionDev.py
+++ ScorpionDev.py
@@ -7,6 +7,8 @@ import time
 from builtins import __import__
 from _ast import Await
 from cat import getCat
+from discord.game import Game
+from tkinter.constants import CURRENT
 __import__
 from googleapiclient.discovery import build
 
@@ -50,7 +52,7 @@ async def on_ready():
 async def on_message(message):
     if message.author != client.user: # Check to make sure the bot can't trigger its own commands
         #Command's contents are stored in commandContents
-        if message.content.startswith(prefix):
+        if message.content.startswith(prefix) or message.content.startswith(adminPrefix):
             commandString = message.content
             commandContents = commandString.split(' ', 1)[-1]
             #React to the command with :scorpion:
@@ -62,7 +64,7 @@ async def on_message(message):
             await client.send_message(message.author, '------------------------\n__**Commands**__\n\n'
                                       +'`cat`\nFinds a random image of a cat\n\n'
                                       +'`info`\nGet info related to the bot\n\n'
-                                      +'`invite`\nGenerate an invite link to the current channel\n\n'
+                                      +'`invite`\nInvite people to the current channel, or invite Scorpion Bot to your server.\n\n'
                                       +'`greet`\nSay hello to the bot\n\n'
                                       +'`google`\nGoogle search for something\n\n'
                                       +'`ping`\nPings the bot and checks to see if it is online\n\n'
@@ -78,17 +80,6 @@ async def on_message(message):
             #Pring "Pong!" alongside the user's username and ID
             print("Pong!", message.author, message.author.id)
             
-        #Command to shut down the bot
-        elif message.content.startswith(adminPrefix+'shutdown'):
-            if message.author.id in adminList:
-                await client.send_message(message.channel, 'Shutting down...')
-                await client.change_presence(game=None,status=None,afk=False)
-                time.sleep(2) # sleep for 2 seconds before shutdown, just to make sure status changed correctly
-                await client.logout()
-                print("Shutting down at", message.author, "'s request") 
-            elif message.author.id not in adminList:
-                await client.send_message(message.channel, 'Error: You are not a bot admin, '+message.author.mention)
-                print(message.author, "tried to shutdown the bot but it not admin")
         
         #Command to ping the bot (again)
         elif message.content.startswith(prefix+'greet'):
@@ -97,7 +88,9 @@ async def on_message(message):
             
         #Command to generate an invite
         elif message.content.startswith(prefix+'invite'):
-            await client.send_message(message.channel, await client.create_invite(message.channel))
+            linkString = str(await client.create_invite(message.channel)) 
+            await client.send_message(message.channel, '__Server Invite:__ <'+linkString+'>'
+                                      +'\n__Bot Invite:__ <https://goo.gl/1SAbnr>')
             print("Created server invite at", message.author, "'s request")
         
         #Command to link to a specified subreddit
@@ -107,7 +100,8 @@ async def on_message(message):
           
         #Command to give information about the bot  
         elif message.content.startswith(prefix+'info'):
-            await client.send_message(message.author, "Scorpion bot is a little bot made by Alstro20 and EmeraldOrbis. Check out the Github project at https://github.com/Alstro20/ScorpiusStingBot")
+            await client.send_message(message.author, 'Scorpion bot is a little bot made by Alstro20 and EmeraldOrbis. Check out the Github project at <https://goo.gl/1SAbnr>'
+                                      +'\nAlso you should come hang out with us. http://discord.gg/vuT5xc8')
             await client.send_message(message.channel, "Sent you a DM, " + message.author.mention)
             print("Info sent to", message.author) 
             
@@ -150,6 +144,38 @@ async def on_message(message):
             #delete the cat picture after sending it
             os.remove(randomCat)
             print("Sent cat image at", message.author, "'s request. -", randomCat)
+            
+        #Command to give a link Let Me Google That For You (LMGTFY)
+        elif message.content.startswith(prefix+'lmgtfy') or message.content.startswith(prefix+'LMGTFY'):
+            print("Googled", commandContents, "for", message.author)
+            #Replace the spaces in commandContents with +'s
+            lmgtfyString = commandContents.replace(" ", "+")
+            #Output the link
+            await client.send_message(message.channel, message.author.mention+', Let me google that for you.\n<http://lmgtfy.com/?q='+lmgtfyString+'>')
+            
+        #[---ADMIN COMMANDS---]
+        #Only users inside the adminList can use the following commands
+        elif message.content.startswith(adminPrefix) and message.author.id in adminList:
+                #Command to shut down the bot
+            if message.content.startswith(adminPrefix+'shutdown'):
+                await client.send_message(message.channel, 'Shutting down...')
+                await client.change_presence(game=None,status=None,afk=False)
+                time.sleep(2) # sleep for 2 seconds before shutdown, just to make sure status changed correctly
+                await client.logout()
+                print("Shutting down at", message.author, "'s request") 
+                
+            #Change the bot's current game with an admin command    
+            elif message.content.startswith(adminPrefix+'changeGame'):
+                #String to store the bot's current game
+                currentGame = commandContents
+                print("Changing current game to", commandContents, "at the admin request of", message.author)
+                await client.change_presence(game=discord.Game(name=currentGame), status=None, afk=False,)
+                
+                
+            #User failed to authenticate (PUT ALL ADMIN COMMANDS BEFORE THIS ONE)
+            elif message.author.id not in adminList:
+                    await client.send_message(message.channel, 'Error: You are not a bot admin, '+message.author.mention)
+                    print(message.author, "tried to shutdown the bot but it not admin")
             
                             
         #Lets user know if script is unknown. Put all commands before this.
